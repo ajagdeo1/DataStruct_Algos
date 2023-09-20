028a604e634c8df14b5c2bceb6648d8b33d781a3
Adrian <a.jagdeo33@gmail.com>
2023-09-20 02:18:37 -0400

    union find update

--- UnionFind/src/WeightedQuickUnion.java
+++ UnionFind/src/WeightedQuickUnion.java
@@ -1,29 +1,73 @@
-public class WeightedQuickUnion {
-    private int[] id;
-    private int[] sz;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
-    public WeightedQuickUnion(int N){
-        for (int i = 0; i < N; i++){
-            id = new int[N];
-            id[i] = i;
+import java.util.Arrays;
+
+public class WeightedQuickUnion {
+  private int [] parent;
+  private int [] size; // number of elements in subtree rooted at i
+  private int count;   // number of components
+
+     public WeightedQuickUnion(int n){
+        count = n;
+        parent = new int[n];
+        size = new int[n];
+
+        for (int i = 0; i < n; i++){
+            parent[i] = i;
+            size[i] = i;
         }
+         System.out.println(Arrays.toString(parent));
     }
-    private int root(int i){
-        while(id[i]!=i) i = id[i];
-        return i;
+    public int count(){
+         return count;
     }
-    public boolean find(int p, int q){
-        return root(p) == root(q);
+    public void validate(int p){
+         int n = parent.length;
+         if(p < 0 || p >= n){
+             throw new IllegalArgumentException("index " + p + " is not between 0 and " + (n-1));
+         }
     }
-    public void weightedUnion(int p, int q){
-        int i = root(p);
-        int j = root(q);
-        if(i == j) return;
-        if(sz[i] < sz[j]){
-            id[i] = j; sz[j] += sz[i];
+    public int find(int p){
+         validate(p);
+         while (p!=parent[p]){
+             p = parent[p];
+         }
+         return p;
+    }
+    public boolean connected(int p, int q){
+         return find(p) == find(q);
+    }
+    public void union(int p, int q){
+         int rootP = find(p);
+         int rootQ = find(q);
+         if(rootP == rootQ){
+             return;
+         }
+         if(size[rootP] < size[rootQ]){
+             parent[rootP] = rootQ;
+             size[rootQ] += size[rootP];
+         }
+         else{
+             parent[rootQ] = rootP;
+             size[rootP] += rootQ;
+         }
+         count --;
+        System.out.println(Arrays.toString(parent));
+    }
+
+    public static void main(String[] args){
+        int n = StdIn.readInt();
+        WeightedQuickUnion UF = new WeightedQuickUnion(n);
+        while(!StdIn.isEmpty()){
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if(UF.find(p) == UF.find(q)){
+                continue;
+            }
+            UF.union(p,q);
+            StdOut.println(p + " " + q);
         }
-        else{
-            id[j] = i; sz[i] += sz[j];
-        }
+        StdOut.println(UF.count() + " components");
     }
 }
